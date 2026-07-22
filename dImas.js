d8e7e2645ddadaebca101a8704ffe013d03f0efb
Dimas Ardian X RPL 6 <dimas_ardian_35rpl@student.smktelkom-mlg.sch.id>
2026-07-22 09:42:36 +0700

    Add files via upload

---
+++ dImas.js
@@ -0,0 +1,5 @@
+console.log("nama : Dimas Ardian");
+console.log("tanggal lahir : 09 Juli 2009");
+console.log("smp : SMP Negeri 21 Malang");
+console.log("saya masuk smk telkom malang karena saya ingin belajar tentang komputer dan saya ingin menjadi seorang programmer");
+console.log("hobi saya adalah bermain game dan membaca buku");
