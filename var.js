f7fece226d69ec68bbc96ec23c0afc57f9a6730a
Dimas Ardian X RPL 6 <dimas_ardian_35rpl@student.smktelkom-mlg.sch.id>
2026-07-23 08:42:51 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,18 @@
+//deklarasi variabel
+let angka = 10; //Number
+let sekolah = "SMK Telkom Malang"; //String
+let kondisi = true; //Boolean
+let data; //undefined
+let nama = null; //null
+
+console.log(typeof angka);
+console.log(typeof sekolah);
+console.log(typeof kondisi);
+console.log(typeof data);
+console.log(typeof nama);
+
+console.log(`ini adalah angka : ${angka}`);
+console.log(`ini adalah sekolah : ${sekolah}`);
+console.log(`ini adalah kondisi : ${kondisi}`);
+console.log(`ini adalah data : ${data}`);
+console.log(`ini adalah nama : ${nama}`);
