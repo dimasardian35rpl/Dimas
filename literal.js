e458e98c52b179c20f6dc1dd75396e0b05e9a4b5
Dimas Ardian X RPL 6 <dimas_ardian_35rpl@student.smktelkom-mlg.sch.id>
2026-07-23 08:38:29 +0700

    Add files via upload

---
+++ literal.js
@@ -0,0 +1,6 @@
+// template literal
+
+const name = "Dimas Ardian";
+const sekolah = "SMK Telkom Malang";
+
+console.log(`Nama saya ${name} dan saya sekolah di ${sekolah}`);
---
+++ variabel.js
@@ -0,0 +1,16 @@
+// variabel.js
+let skor = 100; //number
+let level = "Pemula"; //String
+let nyawa = 13; //Number
+
+const nama = "Dimas Ardian"; //String
+
+console.log(`Skor: ${skor}`);
+console.log(`Level: ${level}`);
+console.log(`Nyawa: ${nyawa}`);
+console.log(`Nama: ${nama}`);
+
+console.log(`Tipe data skor: ${typeof skor}`);
+console.log(`Tipe data level: ${typeof level}`);
+console.log(`Tipe data nyawa: ${typeof nyawa}`);
+console.log(`Tipe data nama: ${typeof nama}`);
